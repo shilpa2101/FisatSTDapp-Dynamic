68c5f4a4b76ef3657a4a07e857aeae9e13de764e
unknown <shilpas2101@gmail.com>
2024-06-09 18:19:52 +0530

    uploaded

---
+++ src/components/View.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const View = () => {
+    const [stdata,changedata]=useState(
+       [
+
+       ]
+
+    )
+    const fetchData=()=>{
+    axios.get("https://anishpdm.github.io/dummy-api-new/student.json").then(
+        (response)=>{
+            changedata(response.data)
+            console.log(response.data)
+        }
+    ).catch(
+        (error)=>{
+            console.log(error.message)
+
+        }
+    ).finally()
+}
+useEffect(
+    ()=>{fetchData()},[]
+)
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-13 col-xxl-12">
+                <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">First NAme</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">College</th>
+                            <th scope="col">dob</th>
+                            <th scope="col">course</th>
+                            <th scope="col">mobile</th>
+                            <th scope="col">email</th>
+                            <th scope="col">address</th>
+                            </tr>
+                        </thead>
+                        {stdata.map(
+                            (value,index)=>{
+                                return  <tbody>
+                                        <tr>
+                                        <th scope="row">{value._id}</th>
+                                        <td>{value.firstname}</td>
+                                        <td>{value.lastname}</td>
+                                        <td>{value.college}</td>
+                                        <td>{value.dob}</td>
+                                        <td>{value.course}</td>
+                                        <td>{value.mobile}</td>
+                                        <td>{value.email}</td>
+                                        <td>{value.address}</td>
+                                        </tr>                        
+                                    </tbody>
+                            }
+                        )}
+                       
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default View
